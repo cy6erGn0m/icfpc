7ca28eb2d2703ff629b685e49a159a99645bb0e5
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2012-07-13 19:31:01 +0400

    Extracted function.

--- test/io/TestReading.kt
+++ test/io/TestReading.kt
@@ -2,9 +2,8 @@ package io
 
 import java.io.File
 import java.io.FileInputStream
-import junit.framework.ComparisonFailure
 import junit.framework.TestCase
-import util.repeat
+import util.assertSameLines
 
 public class TestReading : TestCase() {
     public fun testReadAllMines() {
@@ -13,21 +12,7 @@ public class TestReading : TestCase() {
                 val input = FileInputStream(f)
                 val mine = readMine(input)
                 input.close()
-                val fileText = f.readText()
-                val expectedLines = fileText.split('\n')
-                val toStringText = mine.toString()
-                val actualLines = toStringText.split('\n')
-                for (i in 0..(Math.max(expectedLines.size, actualLines.size) - 1)) {
-                    var expectedLine = if (i < expectedLines.size) expectedLines[i] else ""
-                    var actualLine = if (i < actualLines.size) actualLines[i] else ""
-                    val len = Math.max(expectedLine.length, actualLine.length)
-
-                    expectedLine += " ".repeat(len - expectedLine.length)
-                    actualLine += " ".repeat(len - actualLine.length)
-                    if (expectedLine != actualLine) {
-                        throw ComparisonFailure(null, fileText, toStringText)
-                    }
-                }
+                assertSameLines(f.readText(), mine.toString())
             }
         }
     }
---
+++ test/util.kt
@@ -0,0 +1,19 @@
+package util
+
+import junit.framework.ComparisonFailure
+
+public fun assertSameLines(expected : String, actual : String) {
+    val expectedLines = expected.split('\n')
+    val actualLines = actual.split('\n')
+    for (i in 0..(Math.max(expectedLines.size, actualLines.size) - 1)) {
+        var expectedLine = if (i < expectedLines.size) expectedLines[i] else ""
+        var actualLine = if (i < actualLines.size) actualLines[i] else ""
+        val len = Math.max(expectedLine.length, actualLine.length)
+
+        expectedLine += " ".repeat(len - expectedLine.length)
+        actualLine += " ".repeat(len - actualLine.length)
+        if (expectedLine != actualLine) {
+            throw ComparisonFailure(null, expected, actual)
+        }
+    }
+}
