ea58517cc8f554ba1ecbe68a96758ce7198f45d1
Andrey Breslav <andrey.breslav@jetbrains.com>
2012-07-13 18:28:00 +0400

    Mine diff

--- src/model/map.kt
+++ src/model/map.kt
@@ -1,6 +1,7 @@
 package model
 
 import java.util.HashMap
+import util._assert
 
 val validStates = hashSet(
                     MineCell.ROBOT,
@@ -95,4 +96,17 @@ public class Mine(val width: Int, val height: Int) {
         }
         return sb.toString()!!
     }
+}
+
+fun mineDiff(a: Mine, b: Mine): #(Int, Int)? {
+    _assert(a.width == b.width, "Widths don't match: ${a.width} != ${b.width}")
+    _assert(a.height == b.height, "Heights don't match: ${a.height} != ${b.height}")
+    for (y in 0..a.height - 1) {
+        for (x in 0..a.width - 1) {
+            if (a[x, y] != b[x, y]) {
+                return #(x, y)
+            }
+        }
+    }
+    return null
 }
