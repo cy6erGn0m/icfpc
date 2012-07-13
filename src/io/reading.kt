db855a55c15f083d7d81a4acfede141c71ea3a66
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2012-07-13 18:19:11 +0400

    Added reading mine.

---
+++ src/io/reading.kt
@@ -0,0 +1,27 @@
+package io
+
+import java.io.InputStream
+import java.util.ArrayList
+import java.util.List
+import model.Mine
+import model.toMineCell
+import model.MineCell
+
+public fun readMine(input : InputStream) : Mine {
+    val lines = ArrayList<String>()
+    input.reader().buffered().forEachLine { line ->
+        lines.add(line)
+    }
+    val height = lines.size()
+    val lengths: List<Int> = lines.map { it -> it.length }
+    val width = lengths.fold(0, { x, y -> Math.max(x, y) })
+
+    val mine = Mine(height, width)
+    for (y in 0..(height - 1)) {
+        val line = lines[height - y - 1]
+        for (x in 0..(width - 1)) {
+            mine[x, y] = if (x < line.length()) line[x].toMineCell() else MineCell.EMPTY
+        }
+    }
+    return mine
+}
