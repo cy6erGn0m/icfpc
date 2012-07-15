b172c2f0d9e3f6278ddedd7a8f8088cb458b05ef
Evgeny Gerashchenko <Evgeny.Gerashchenko@jetbrains.com>
2012-07-15 15:26:12 +0400

    Moved Mine.toString() to io directory (converted to extension function).

---
+++ src/io/write.kt
@@ -0,0 +1,60 @@
+package io
+
+import java.util.HashMap
+import model.Point
+import model.MineCell
+import model.Mine
+
+public fun Mine.toString() : String {
+    val sb = StringBuilder()
+    var unresolvedTrampolinesToLocations = HashMap<Char, Point>()
+    for (yy in 0..height - 1) {
+        val y = height - yy - 1
+        for (x in 0..width - 1) {
+            val cell = this[x, y]
+            if (cell == MineCell.TRAMPOLINE || cell == MineCell.TARGET) {
+                val location = Point(x, y)
+                val id = trampolinesMap.getId(location)
+                if (cell == MineCell.TRAMPOLINE) {
+                    unresolvedTrampolinesToLocations[id] = location
+                }
+                sb.append(id)
+            }
+            else {
+                sb.append(cell)
+            }
+        }
+        sb.append("\n")
+    }
+
+    val waterMetadataPresent = !(water == 0 && floodPeriod == 0 && waterproof == 10)
+    val trampolinesMetadataPresent = !unresolvedTrampolinesToLocations.isEmpty()
+    val beardMetadataPresent = !(beardGrowthPeriod == 25 && razors == 0)
+
+    if (waterMetadataPresent || trampolinesMetadataPresent || beardMetadataPresent) {
+        sb.append("\n")
+    }
+
+    if (waterMetadataPresent) {
+        sb.append("Water ${water + 1}\n")
+        sb.append("Flooding $floodPeriod${ if (floodPeriod != nextFlood) "/" + nextFlood else "" }\n")
+        sb.append("Waterproof $waterproof\n")
+    }
+
+    if (trampolinesMetadataPresent) {
+        for (trampolineId in 'A'..'I') {
+            val trampolineLocation = unresolvedTrampolinesToLocations[trampolineId]
+            if (trampolineLocation == null) continue
+            val targetLocation = trampolinesMap.getTarget(trampolineLocation)
+            val targetId = trampolinesMap.getId(targetLocation)
+            sb.append("Trampoline $trampolineId targets $targetId\n")
+        }
+    }
+
+    if (beardMetadataPresent) {
+        sb.append("Growth $beardGrowthPeriod${ if (beardGrowthPeriod != nextBeardGrowth) "/" + nextBeardGrowth else "" }\n")
+        sb.append("Razors $razors\n")
+    }
+
+    return sb.toString()!!
+}
--- src/model/mine.kt
+++ src/model/mine.kt
@@ -191,60 +191,6 @@ public class Mine(private val matrix: CellMatrix, public val trampolinesMap: Tra
 
     private fun inRange(x: Int, y: Int) = x in 0..(width - 1) && y in 0..(height - 1)
 
-    public fun toString(): String {
-        val sb = StringBuilder()
-        var unresolvedTrampolinesToLocations = HashMap<Char, Point>()
-        for (yy in 0..height - 1) {
-            val y = height - yy - 1
-            for (x in 0..width - 1) {
-                val cell = this[x, y]
-                if (cell == MineCell.TRAMPOLINE || cell == MineCell.TARGET) {
-                    val location = Point(x, y)
-                    val id = trampolinesMap.getId(location)
-                    if (cell == MineCell.TRAMPOLINE) {
-                        unresolvedTrampolinesToLocations[id] = location
-                    }
-                    sb.append(id)
-                }
-                else {
-                    sb.append(cell)
-                }
-            }
-            sb.append("\n")
-        }
-
-        val waterMetadataPresent = !(water == 0 && floodPeriod == 0 && waterproof == 10)
-        val trampolinesMetadataPresent = !unresolvedTrampolinesToLocations.isEmpty()
-        val beardMetadataPresent = !(beardGrowthPeriod == 25 && razors == 0)
-
-        if (waterMetadataPresent || trampolinesMetadataPresent || beardMetadataPresent) {
-            sb.append("\n")
-        }
-
-        if (waterMetadataPresent) {
-            sb.append("Water ${water + 1}\n")
-            sb.append("Flooding $floodPeriod${ if (floodPeriod != nextFlood) "/" + nextFlood else "" }\n")
-            sb.append("Waterproof $waterproof\n")
-        }
-
-        if (trampolinesMetadataPresent) {
-            for (trampolineId in 'A'..'I') {
-                val trampolineLocation = unresolvedTrampolinesToLocations[trampolineId]
-                if (trampolineLocation == null) continue
-                val targetLocation = trampolinesMap.getTarget(trampolineLocation)
-                val targetId = trampolinesMap.getId(targetLocation)
-                sb.append("Trampoline $trampolineId targets $targetId\n")
-            }
-        }
-
-        if (beardMetadataPresent) {
-            sb.append("Growth $beardGrowthPeriod${ if (beardGrowthPeriod != nextBeardGrowth) "/" + nextBeardGrowth else "" }\n")
-            sb.append("Razors $razors\n")
-        }
-
-        return sb.toString()!!
-    }
-
     public fun copy(): Mine {
         val copy = Mine(width, height, trampolinesMap)
         for (x in 0..width - 1) {
