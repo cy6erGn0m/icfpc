ad9825fe971f60f55472bfea1df8e8ead69bb8bc
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2012-07-15 21:04:22 +0400

    TRAMPOLINE changed to TARGET in makeMove

--- src/evaluator/makeMove.kt
+++ src/evaluator/makeMove.kt
@@ -46,7 +46,7 @@ fun makeMove(move: Move, robot: Robot, update: (Mine) -> Mine): Robot {
             oldMine[target] = EMPTY
             newPos = target
         }
-        WALL, CLOSED_LIFT, INVALID, TRAMPOLINE, EARTH, EMPTY, ROBOT -> {
+        WALL, CLOSED_LIFT, INVALID, TARGET, EARTH, EMPTY, ROBOT -> {
             //nothing to do here
         }
         LAMBDA -> {
